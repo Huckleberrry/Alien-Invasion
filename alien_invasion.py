570f9a7fd29c35480a1af5eb33bef87ce4e2cbf4
Steven Vargas <githuckleberry@gmail.com>
2023-11-02 10:45:16 -0700

    Game Functions - Creating Fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,6 @@
 # Import modules
-import sys
 import pygame
 from pygame.sprite import Group
-from alien import Alien
-
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -17,14 +14,15 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
 
-    # Make an alien.
-    alien = Alien(ai_settings, screen)
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
+
 
 
     # Start the main loop for the game.
@@ -33,9 +31,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
-        
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
 
