bf9b8ec3a28c4c0e09340351615248b9df2da124
Steven Vargas <githuckleberry@gmail.com>
2023-11-03 11:53:09 -0700

    Fire bullet bug fix

--- game_functions.py
+++ game_functions.py
@@ -12,16 +12,13 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        fire_bullet(ai_settings, screen, ship, bullets)
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
     elif event.key == pygame.K_q:
         sys.exit()
 
-def fire_bullet(ai_settings, screen, ship, bullets):
-        """Fire a bullet if limit not reached yet."""
-    # Create a new bullet and add it to the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
@@ -51,7 +48,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 
     # Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
-        bullet.draw_bullet()
+         bullet.draw_bullet()
     ship.blitmet()
     aliens.draw(screen)
     # Make the most revently drawn screen visible.
@@ -60,11 +57,12 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 def update_bullets(bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet positions.
+    bullets.update()
     # Get rid of bullets that have disappeared.
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        print(len(bullets))
+             bullets.remove(bullet)
+    print(len(bullets))
 
 
 def get_number_aliens_x(ai_settings, alien_width):
